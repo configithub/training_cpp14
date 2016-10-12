0038f61cde9002c8c331b807eeb99f08220384b4
Gilles <gmasselot@gmail.com>
2016-10-12 14:20:25 +0200

    move sem

--- memory/move_semantics.cpp
+++ memory/move_semantics.cpp
@@ -24,17 +24,13 @@ struct unique_array {
     _buffer = buf_ptr;
     _size = s;
     std::cout << "moving buffer" << std::endl;
+    return *this;
   } 
   // move constructor
-  unique_array(unique_array&& a) {
-    // swap buffers and size value between the two objects
-    // do not copy the buffer
-    char* buf_ptr = a._buffer;
-    size_t s = a._size;
-    a._buffer = _buffer;
-    a._size = _size;
-    _buffer = buf_ptr;
-    _size = s;
+  unique_array(unique_array&& a) : _buffer(nullptr), _size(0) { 
+    // initialization at nullptr and 0 avoids a segfault
+    std::swap(_buffer, a._buffer); // swap 2 variables, uses move if possible
+    std::swap(_size, a._size);
     std::cout << "move construction" << std::endl;
   } 
 
@@ -55,15 +51,19 @@ struct unique_array {
 
 int main(int, char**) {
   unique_array a(5);
-  unique_array b(6);
 
   // unique_array& b = std::move(a); // does not compile : copy is prevented
   // const unique_array& b = std::move(a); // ok
+  
+  unique_array b(6);
   b = std::move(a); // b and a are swapped, 
-  // the move assignment defined in unique_array is used
-  // a shouldn't be used after this 
-  std::cout << "size of b " <<  b.size() << std::endl;
+  //   the move assignment defined in unique_array is used
+  //   a shouldn't be used after this 
+  std::cout << "size of b: " <<  b.size() << std::endl;
+  
+  unique_array c(std::move(b)); // calls the move constructor
+  
+  std::cout << "size of c: " <<  c.size() << std::endl;
 
-  unique_array c(std::move(b));
 
 }
